2d41cb476531e0e0f11efd222ff870ff54308f26
Konrad Ciepluch <konrad190008@wp.pl>
2021-05-26 13:18:35 +0200

    App and index change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Text from './Text';
 
 class App extends Component {
+  state = {
+    underline: false
+  }
+
+  handleClick = () => {
+    this.setState({
+      underline: !this.state.underline,
+    })
+  }
+
   render() {
+     const text = 'Witaj na stronie';
+  /* let classes = '';
+     if(this.state.underline) classes+= ' line';
+    if(text.length > 10) classes+= ' big'
+ */
+
+    const classes = ['one'];
+    if(this.state.underline) classes.push('line');
+    if(text.length > 10) classes.push('big')
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='app' onClick={this.handleClick}>
+        <h1 className={classes.join(' ')}>{text}</h1>
+        <Text />
       </div>
     );
   }
