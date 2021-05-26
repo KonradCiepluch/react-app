d4ee2ebf014bed025d48effb511feec9269ebd61
Konrad Ciepluch <konrad190008@wp.pl>
2021-05-26 13:44:19 +0200

    Updates

---
+++ precache-manifest.f6c0f4a4d43f1d6e655be7e51aca904f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a4021c71153d917c941",
+    "url": "/react-app/static/css/main.366ba280.chunk.css"
+  },
+  {
+    "revision": "7a4021c71153d917c941",
+    "url": "/react-app/static/js/main.114924f5.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/react-app/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "b3c0f9f9a8f022510c57e9f6467552b9",
+    "url": "/react-app/index.html"
+  }
+];
